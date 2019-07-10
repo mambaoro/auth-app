65a1eccf1b2cd6be06e87dc35c2d62ac15b2c94b
Baoro <mam.baoro@outlook.fr>
2019-07-11 01:47:15 +0200

    no cache fix

--- server/index.js
+++ server/index.js
@@ -26,14 +26,12 @@ const app = express();
 app.set('trust proxy', 1);
 app.use(cors());
 
-const nocache = (req, res, next) => {
+app.use((req, res, next) => {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
   next();
-};
-
-app.use(nocache());
+});
 
 const options = {
   host: process.env.DATABASE_HOST,
